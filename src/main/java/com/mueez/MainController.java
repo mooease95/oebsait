1c4551aa52e72c137923610687ab224e9bd616c0
Mueez Abdur-Rahman <mueez.abdurrahman@gmail.com>
2025-10-28 22:19:51 +0000

    (Fixed) Used Thymeleaf to separate header into another file. Fixed by removing Thymeleaf version.

--- src/main/java/com/mueez/MainController.java
+++ src/main/java/com/mueez/MainController.java
@@ -14,31 +14,31 @@ public class MainController {
 
     @GetMapping("/about")
     public String about() {
-        return "default-error-page.html";
+        return "default-error-page";
     }
 
     @GetMapping("/engineering")
     public String engineering() {
-        return "default-error-page.html";
+        return "default-error-page";
     }
 
     @GetMapping("/sports")
     public String sports() {
-        return "default-error-page.html";
+        return "default-error-page";
     }
 
     @GetMapping("/food")
     public String food() {
-        return "default-error-page.html";
+        return "default-error-page";
     }
 
     @GetMapping("/politics")
     public String politics() {
-        return "default-error-page.html";
+        return "default-error-page";
     }
 
     @GetMapping("/contact")
     public String contact() {
-        return "default-error-page.html";
+        return "default-error-page";
     }
 }
