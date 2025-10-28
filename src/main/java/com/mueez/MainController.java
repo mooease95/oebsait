cf6eca4655c80a20aaa3ef63e74ece8d9434981a
Mueez Abdur-Rahman <mueez.abdurrahman@gmail.com>
2025-10-28 20:50:35 +0000

    Added endpoints for nav links, all pointing to an error page for now.

--- src/main/java/com/mueez/MainController.java
+++ src/main/java/com/mueez/MainController.java
@@ -10,4 +10,34 @@ public class MainController {
     public String home() {
         return "landing-page.html";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "default-error-page.html";
+    }
+
+    @GetMapping("/engineering")
+    public String engineering() {
+        return "default-error-page.html";
+    }
+
+    @GetMapping("/sports")
+    public String sports() {
+        return "default-error-page.html";
+    }
+
+    @GetMapping("/food")
+    public String food() {
+        return "default-error-page.html";
+    }
+
+    @GetMapping("/politics")
+    public String politics() {
+        return "default-error-page.html";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "default-error-page.html";
+    }
 }
