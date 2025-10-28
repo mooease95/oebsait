9d664daab7401b32297a07062a03ac21357d6337
Mueez Abdur-Rahman <mueez.abdurrahman@gmail.com>
2025-10-28 22:14:14 +0000

    (Broken) Using Thymeleaf to separate the header into another file.

--- src/main/java/com/mueez/MainController.java
+++ src/main/java/com/mueez/MainController.java
@@ -1,14 +1,15 @@
 package com.mueez;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String home() {
-        return "landing-page.html";
+    public String home(Model model) {
+        return "landing-page";
     }
 
     @GetMapping("/about")
